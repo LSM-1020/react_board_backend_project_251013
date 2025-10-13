aa08f059fb6b667406b0d3f3adc9ac30e6269f39
LSM-1020 <smmlee@naver.com>
2025-10-13 11:14:53 +0900

    리액트 보드 백엔드 초기세팅

---
+++ src/main/java/com/LSM/home/ReactBoardBackendProject251013Application.java
@@ -0,0 +1,13 @@
+package com.LSM.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactBoardBackendProject251013Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactBoardBackendProject251013Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/LSM/home/ReactBoardBackendProject251013ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LSM.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactBoardBackendProject251013ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
